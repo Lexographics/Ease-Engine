dcad1ba7a189d7cb13a9c7d45cb2171133f34ec9
Lexographics <zbamgamesinfo@gmail.com>
2024-05-01 21:12:19 +0300

    add node hierarchy

---
+++ src/scene/node.cpp
@@ -0,0 +1,66 @@
+#include "node.hpp"
+
+void Node::RemoveChild(Node *child) {
+	child->_parent = nullptr;
+	removeChild(child);
+}
+
+void Node::AddChild(Node *child) {
+	if (child->_parent != nullptr) {
+		child->_parent->removeChild(child);
+	}
+	child->_parent = this;
+	_children.push_back(child);
+}
+
+size_t Node::GetChildCount() {
+	return _children.size();
+}
+
+Node *Node::GetChild(size_t index) {
+	if (_children.size() >= index)
+		return nullptr;
+	return _children[index];
+}
+
+Node *Node::GetNode(const std::string nodePath, bool recursive) {
+	std::filesystem::path path(nodePath);
+
+	if (recursive) {
+		auto nodePaths = Utils::Split(nodePath, "/");
+		Node *node = this; // todo: if string starts with /, start it from root node
+		for (auto path : nodePaths) {
+			node = node->GetNode(path, false);
+			if (nullptr == node)
+				break;
+		}
+		return node;
+	}
+
+	for (Node *child : _children) {
+		if (child->Name() == nodePath)
+			return child;
+	}
+	return nullptr;
+}
+
+static void PrintIndent(int count) {
+	for (int i = 0; i < count; i++) {
+		std::cout << "=";
+	}
+	std::cout << "> ";
+}
+
+void Node::PrintHierarchy(int indent) {
+	PrintIndent(indent);
+	std::cout << Name() << std::endl;
+
+	Node *node = this;
+	for (Node *child : GetChildren()) {
+		child->PrintHierarchy(indent + 1);
+	}
+}
+
+void Node::removeChild(Node *child) {
+	_children.erase(std::remove(_children.begin(), _children.end(), child), _children.end());
+}
--- src/scene/node.hpp
+++ src/scene/node.hpp
@@ -2,9 +2,13 @@
 #define NODE_HPP
 #pragma once
 
+#include <algorithm>
+#include <iostream>
 #include <string>
+#include <vector>
 
 #include "sowa.hpp"
+#include "utils/utils.hpp"
 
 class Node {
   public:
@@ -15,7 +19,23 @@ class Node {
 	inline const std::string &Name() const { return _name; }
 	inline void Rename(const std::string &name) { _name = name; }
 
+	inline std::vector<Node *> GetChildren() { return _children; }
+	inline Node *GetParent() { return _parent; }
+
+	void RemoveChild(Node *child);
+	void AddChild(Node *child);
+
+	size_t GetChildCount();
+	Node *GetChild(size_t index);
+
+	Node *GetNode(const std::string nodePath, bool recursive = true);
+
+	void PrintHierarchy(int indent = 0);
+
   private:
+	// Internal hierarchy functions that does not modify other than the node passed
+	void removeChild(Node *child);
+
 	friend class Scene;
 	friend class NodeDB;
 
@@ -23,6 +43,9 @@ class Node {
 	NodeID _id = 0;
 
 	std::string _name = "";
+
+	Node *_parent = nullptr;
+	std::vector<Node *> _children;
 };
 
 #endif // NODE_HPP
