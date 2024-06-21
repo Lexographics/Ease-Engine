4816e4b19afed84cbd1dddd2752b1c2098e3b0e3
Lexographics <zbamgamesinfo@gmail.com>
2024-06-21 15:38:53 +0300

    add matrix projection && decompose transform

--- src/math/matrix.cpp
+++ src/math/matrix.cpp
@@ -1,6 +1,8 @@
 #include "matrix.hpp"
 
-#include <glm/gtc/matrix_transform.hpp>
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/quaternion.hpp"
+#include "glm/gtx/matrix_decompose.hpp"
 
 glm::mat4 Matrix::CalculateTransform(glm::vec2 position, float rotation, glm::vec2 scale, const glm::mat4 &base /*= glm::mat4(1.f)*/) {
 	glm::mat4 transform = glm::translate(base, {position.x, position.y, 0.f});
@@ -18,4 +20,29 @@ glm::mat4 Matrix::CalculateTransform3D(glm::vec3 position, glm::vec3 rotation, g
 	transform = glm::scale(transform, {scale.x, scale.y, scale.z});
 
 	return transform;
+}
+
+bool Matrix::DecomposeTransform(const glm::mat4 &mat, glm::vec2 *position, float *rotation, glm::vec2 *scale) {
+	glm::vec3 _position;
+	glm::vec3 _scale;
+	glm::quat _orientation;
+	glm::vec3 _skew;
+	glm::vec4 _perspective;
+
+	bool ok = glm::decompose(mat, _scale, _orientation, _position, _skew, _perspective);
+	if (!ok)
+		return false;
+
+	if (position)
+		*position = glm::vec2(_position.x, _position.y);
+	if (rotation)
+		*rotation = glm::degrees(glm::eulerAngles(_orientation).z);
+	if (scale)
+		*scale = glm::vec2(_scale.x, _scale.y);
+
+	return true;
+}
+
+glm::mat4 Matrix::CalculateOrtho(float left, float right, float bottom, float top) {
+	return glm::ortho(left, right, bottom, top, -128.f, 128.f);
 }
