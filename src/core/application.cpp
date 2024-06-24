619741247e2762059b129f5722a8ef086056fd02
Lexographics <zbamgamesinfo@gmail.com>
2024-06-24 16:09:02 +0300

    Register node properties

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -15,9 +15,12 @@
 
 #include <iostream>
 
+#include "scene/node/camera2d.hpp"
 #include "scene/node/node2d.hpp"
 #include "scene/node/sprite2d.hpp"
 #include "scene/node/text2d.hpp"
+
+#include "servers/script_server.hpp"
 
 #include "debug.hpp"
 
@@ -52,64 +55,112 @@ void Application::Init() {
 	Input::InitState(&_window);
 	Visual::InitState(&_window);
 	_renderer.Init();
-	_renderer.RegisterRenderer2D("2D", "res/shaders/sprite2d");
-	_renderer.RegisterRenderer2D("Text", "res/shaders/text2d");
+	_renderer.RegisterRenderer2D("2D", "res://shaders/sprite2d");
+	_renderer.RegisterRenderer2D("Text", "res://shaders/text2d");
 
 	_editor.Init();
 
 	Input::SetActionKeys("ui_accept", {Key::Enter});
 	Input::SetActionKeys("ui_exit", {Key::Escape});
 
+	Input::SetActionKeys("left", {Key::A});
+	Input::SetActionKeys("right", {Key::D});
+	Input::SetActionKeys("up", {Key::W});
+	Input::SetActionKeys("down", {Key::S});
+
 	texture = std::make_unique<ImageTexture>();
 	mesh = std::make_unique<Mesh>();
 
-	texture->Load("tile.png");
-	GetResourceRegistry().AddResource(texture.get());
-	Debug::Info("Texture: {}", *texture);
+	texture->Load("res://tile.png");
 
 	texture->Bind(0);
 
-	mesh->Load("teapot.obj");
+	mesh->Load("res://teapot.obj");
 
-	_defaultFont.LoadTTF("font.ttf");
+	_defaultFont.LoadTTF("res://font.ttf");
 
 	_mainViewport.Create(
 		_projectSettings.rendering.viewport.width,
 		_projectSettings.rendering.viewport.height);
 
-	_nodeDB.NewNodeType<Node>("Node");
-	_nodeDB.NewNodeType<Node2D>("Node2D");
-	_nodeDB.NewNodeType<Sprite2D>("Sprite2D");
-	_nodeDB.NewNodeType<Text2D>("Text2D");
+	NodeTypeID typeId_Node = _nodeDB.NewNodeType<Node>("Node", 0);
+	NodeTypeID typeId_Node2D = _nodeDB.NewNodeType<Node2D>("Node2D", typeId_Node);
+	NodeTypeID typeId_Sprite2D = _nodeDB.NewNodeType<Sprite2D>("Sprite2D", typeId_Node2D);
+	NodeTypeID typeId_Text2D = _nodeDB.NewNodeType<Text2D>("Text2D", typeId_Node2D);
+	NodeTypeID typeId_Camera2D = _nodeDB.NewNodeType<Camera2D>("Camera2D", typeId_Node2D);
+
+	_editor.RegisterNodeProp(
+		typeId_Node2D,
+		EditorNodeProp(
+			"Position",
+			EditorNodePropType::Vec2,
+			[](Node *node) -> void * { return reinterpret_cast<void *>(&dynamic_cast<Node2D *>(node)->Position()); }));
+	_editor.RegisterNodeProp(
+		typeId_Node2D,
+		EditorNodeProp(
+			"Rotation",
+			EditorNodePropType::Float,
+			[](Node *node) -> void * { return reinterpret_cast<void *>(&dynamic_cast<Node2D *>(node)->Rotation()); }));
+	_editor.RegisterNodeProp(
+		typeId_Node2D,
+		EditorNodeProp(
+			"Scale",
+			EditorNodePropType::Vec2,
+			[](Node *node) -> void * { return reinterpret_cast<void *>(&dynamic_cast<Node2D *>(node)->Scale()); }));
+
+	_editor.RegisterNodeProp(
+		typeId_Sprite2D,
+		EditorNodeProp(
+			"Texture",
+			EditorNodePropType::RID,
+			[](Node *node) -> void * { return reinterpret_cast<void *>(&dynamic_cast<Sprite2D *>(node)->GetTexture()); }));
+	_editor.RegisterNodeProp(
+		typeId_Sprite2D,
+		EditorNodeProp(
+			"Modulate",
+			EditorNodePropType::Color,
+			[](Node *node) -> void * { return reinterpret_cast<void *>(&dynamic_cast<Sprite2D *>(node)->Modulate()); }));
+
+	_editor.RegisterNodeProp(
+		typeId_Text2D,
+		EditorNodeProp(
+			"Text",
+			EditorNodePropType::String,
+			[](Node *node) -> void * { return reinterpret_cast<void *>(&dynamic_cast<Text2D *>(node)->Text()); }));
+	_editor.RegisterNodeProp(
+		typeId_Text2D,
+		EditorNodeProp(
+			"Modulate",
+			EditorNodePropType::Color,
+			[](Node *node) -> void * { return reinterpret_cast<void *>(&dynamic_cast<Text2D *>(node)->Modulate()); }));
+	_editor.RegisterNodeProp(
+		typeId_Text2D,
+		EditorNodeProp(
+			"Font",
+			EditorNodePropType::RID,
+			[](Node *node) -> void * { return reinterpret_cast<void *>(&dynamic_cast<Text2D *>(node)->GetFont()); }));
+
+	_editor.RegisterNodeProp(
+		typeId_Camera2D,
+		EditorNodeProp(
+			"Rotatable",
+			EditorNodePropType::Bool,
+			[](Node *node) -> void * { return reinterpret_cast<void *>(&dynamic_cast<Camera2D *>(node)->Rotatable()); }));
+
 	_currentScene = NewScene();
+	_backgroundScene = NewScene();
+	_currentScene->LoadFromFile("res://game.sscn");
 
-	Node *node = _currentScene->Create("Node");
-	Node *child1 = _currentScene->Create("Sprite2D", "Child1");
-	Node *child1child1 = _currentScene->Create("Text2D", "Child1Child1");
-	Node *child2 = _currentScene->Create("Node", "Child2");
-	Node *child2child1 = _currentScene->Create("Node", "Child2Child1");
-
-	node->AddChild(child1);
-	node->AddChild(child2);
-	child2->AddChild(child2child1);
-
-	std::cout << node->GetNode("Child2/Child2Child1")->Name() << std::endl;
-
-	node->GetNode("Child1")->AddChild(child1child1);
-
-	ssss = dynamic_cast<Sprite2D *>(node->GetNode("Child1"));
-	ssss->Position() = {0.f, 0.f};
-	ssss->Scale() = {.06f, .06f};
-	ssss->GetTexture() = texture->GetRID();
-
-	ss = dynamic_cast<Text2D *>(node->GetNode("Child1/Child1Child1"));
-	ss->Text() = "This is a text";
-	ss->Scale() = {20.f, 20.f};
-	ss->ZIndex() = 1;
-
-	Debug::Info("Text node is ::: -> {}", *child1child1);
-
-	node->PrintHierarchy();
+	Input::InputEvent().append([this](Input::Event event) {
+		if (event.type == Input::EventType::Key) {
+			if (event.key.key == Key::F5 && event.key.action == GLFW_PRESS) {
+				if (IsRunning())
+					Stop();
+				else
+					Start();
+			}
+		}
+	});
 
 #ifdef SW_WEB
 	emscripten_set_main_loop_arg(
@@ -132,24 +183,25 @@ void Application::Update() {
 
 	Visual::UseViewport(&_mainViewport);
 
+	if (IsRunning())
+		_scriptServer.CallUpdate();
+
 	_mainViewport.Clear(0.2f, 0.2f, 0.2f, 1.f, true);
 
-	static float f = 0.f;
-	f += 0.2f;
-	glEnable(GL_DEPTH_TEST);
-	GetRenderer().DrawMesh(*mesh, Matrix::CalculateTransform3D({0.f, -20.f, -50.f}, {0.f, f, 0.f}, {10.f, 10.f, 10.f}));
-	glDisable(GL_DEPTH_TEST);
+	// static float f = 0.f;
+	// f += 0.2f;
+	// glEnable(GL_DEPTH_TEST);
+	// GetRenderer().DrawMesh(*mesh, Matrix::CalculateTransform3D({0.f, -20.f, -50.f}, {0.f, f, 0.f}, {10.f, 10.f, 10.f}));
+	// glDisable(GL_DEPTH_TEST);
 
+	if (IsRunning()) {
+		GetRenderer().GetRenderer2D("2D").SetProjectionMatrix(GetCurrentScene()->GetMatrix2D());
+		GetRenderer().GetRenderer2D("Text").SetProjectionMatrix(GetCurrentScene()->GetMatrix2D());
+	} else {
+		GetRenderer().GetRenderer2D("2D").SetProjectionMatrix(_editor.GetCamera2DMatrix());
+		GetRenderer().GetRenderer2D("Text").SetProjectionMatrix(_editor.GetCamera2DMatrix());
+	}
 	GetRenderer().BeginDraw();
-
-	ssss->Position() = {300.f, 400.f};
-	ssss->Rotation() += 0.6f;
-	ss->Rotation() += 0.2f;
-
-	static float d = 0.f;
-	d += 1.f;
-	d = std::fmod(d, 360);
-	ss->Modulate() = Color::HSV(d, 0.5f, 0.5f);
 
 	_currentScene->Update();
 
@@ -164,7 +216,29 @@ void Application::Update() {
 	GetRenderer().DrawFullscreen(_mainViewport.GetTargetTextureID(0));
 	glEnable(GL_DEPTH_TEST);
 
+	_editor.Begin();
+	_editor.Update();
+	_editor.End();
+
 	_window.SwapBuffers();
+}
+
+void Application::Start() {
+	if (_isRunning)
+		return;
+	_isRunning = true;
+	Scene::Copy(_currentScene.get(), _backgroundScene.get());
+
+	_scriptServer.Init();
+	_scriptServer.CallStart();
+}
+
+void Application::Stop() {
+	if (!_isRunning)
+		return;
+	_isRunning = false;
+
+	Scene::Copy(_backgroundScene.get(), _currentScene.get());
 }
 
 Ref<Scene> Application::NewScene() {
--- src/core/application.hpp
+++ src/core/application.hpp
@@ -4,6 +4,7 @@
 
 #include "sowa.hpp"
 
+#include "visual/renderer.hpp"
 #include "visual/viewport.hpp"
 #include "visual/window.hpp"
 
@@ -14,8 +15,12 @@
 #include "scene/node_db.hpp"
 #include "scene/scene.hpp"
 
+#include "servers/script_server.hpp"
+
 #include "resource/font.hpp"
 #include "resource/resource_registry.hpp"
+
+#include "editor/editor.hpp"
 
 class Application {
   public:
@@ -25,26 +30,43 @@ class Application {
 	void Init();
 	void Update();
 
+	void Start();
+	void Stop();
+
+	inline bool IsRunning() { return _isRunning; }
+
 	inline FileSystem *FS() { return _fs; }
 	inline NodeDB &GetNodeDB() { return _nodeDB; }
 	inline ResourceRegistry &GetResourceRegistry() { return _resourceRegistry; }
 	inline Font *GetDefaultFont() { return &_defaultFont; }
+	inline Renderer &GetRenderer() { return _renderer; }
+	inline Window &GetWindow() { return _window; }
+	inline Viewport &GetMainViewport() { return _mainViewport; }
+	inline Editor &GetEditor() { return _editor; }
 
 	Ref<Scene> NewScene();
+	inline Ref<Scene> GetCurrentScene() { return _currentScene; }
 
   private:
+	bool _isRunning = false;
+
 	FileSystem *_fs = nullptr;
 
 	ProjectSettings _projectSettings;
 
 	NodeDB _nodeDB;
 	Ref<Scene> _currentScene;
+	Ref<Scene> _backgroundScene;
+	ScriptServer _scriptServer;
 
+	Renderer _renderer;
 	ResourceRegistry _resourceRegistry;
 	Font _defaultFont;
 
 	Window _window;
 	Viewport _mainViewport;
+
+	Editor _editor;
 };
 
 Application &App();
